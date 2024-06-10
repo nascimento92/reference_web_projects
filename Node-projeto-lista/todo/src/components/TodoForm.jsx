de9774ccc5e1a71adc8e470ae0efd270ca7cbcf1
nascimento92 <gabriel_maker@hotmail.com>
2024-06-10 20:18:03 -0300

    first commit

---
+++ Node-projeto-lista/todo/src/components/TodoForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function TodoForm() {
+  return (
+    <div className="todo-form">
+        <h2>Criar Tarefa:</h2>
+        <form>
+            <input type="text" placeholder='Digite o título' />
+            <select>
+                <option value=""> Selecione uma categoria</option>
+                <option value="Trabalho"> Trabalho</option>
+                <option value="Pessoal"> Pessoal</option>
+                <option value="Estudos"> Estudos</option>
+            </select>
+            <button type='submit'>Criar Tarefa</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
+
+//rfce
