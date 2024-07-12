3bbed0e1e99f2972553f57eadfc0185d6adc196f
Gabriel Nascimento <gabriel_maker@hotmail.com>
2024-07-11 23:24:09 -0300

    new

---
+++ react-vite-typescript/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+      <main className="my-10 w-full bg-red-400 md:max-w-2xl md:bg-green-500">
+        <h1 className="text-4xl font-medium text-white">Clientes</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
