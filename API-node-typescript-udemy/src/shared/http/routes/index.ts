a6405e5757833060d370df28f89f75ca9b08792d
Gabriel Nascimento <gabriel_maker@hotmail.com>
2024-08-19 20:59:07 -0300

    middle

--- API-node-typescript-udemy/src/shared/http/routes/index.ts
+++ API-node-typescript-udemy/src/shared/http/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from 'express';
+import { AppError } from '../error/appError';
 const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
+  throw new AppError('Acesso negado');
   return response.json({ message: 'Olá dev' });
 });
 
--- API-node-typescript-udemy/src/shared/http/server.ts
+++ API-node-typescript-udemy/src/shared/http/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import 'dotenv/config';
 import { routes } from './routes';
+import { AppError } from './error/appError';
 
 const app = express();
 
@@ -10,6 +11,26 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'Error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+      status: 'Error',
+      message: 'Internal server error',
+    });
+
+    next();
+  },
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT} ✨`);
