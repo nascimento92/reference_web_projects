0b561799532496534d466dd6bb5ee53def02b94d
Gabriel Nascimento <gabriel_maker@hotmail.com>
2024-09-01 18:40:07 -0300

    add swagger

--- API-node-typescript-udemy/src/shared/http/server.ts
+++ API-node-typescript-udemy/src/shared/http/server.ts
@@ -1,15 +1,17 @@
 import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
 import 'dotenv/config';
 import { routes } from './routes';
 import { AppError } from './error/appError';
+import swaggerFile from '../../swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 app.use(
